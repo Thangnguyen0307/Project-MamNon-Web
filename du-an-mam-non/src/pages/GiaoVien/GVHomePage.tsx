18639382296b15ecc980e2895f6a16b6da9115f3
HungNguyen <110725585+Dephe1101@users.noreply.github.com>
2025-09-23 20:08:02 +0700

    HN

--- du-an-mam-non/src/pages/GiaoVien/GVHomePage.tsx
+++ du-an-mam-non/src/pages/GiaoVien/GVHomePage.tsx
@@ -1,7 +1,6 @@
 import Footer from "../HomePage2/Footer2";
 import Landing from "../HomePage2/Landing";
 import GV_Header from "./GV_Header";
-import GV_TeachersSection from "./GV_TeachersSection";
 import { Outlet } from "react-router";
 
 export const GVHomePage = () => {
@@ -9,9 +8,6 @@ export const GVHomePage = () => {
     <>
       <GV_Header />
       <Landing heightLanding="450" />
-      {/* Title */}
-      <GV_TeachersSection />
-      {/* List */}
       <section className="bg-white pb-8">
         <Outlet />
       </section>
